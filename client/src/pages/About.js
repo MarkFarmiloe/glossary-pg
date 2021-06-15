3abdf5db658cb33a3882ec82b9eb92a49b481bdf
Mark Farmiloe <mark@farmiloe.me.uk>
2021-06-15 22:16:27 +0100

    About page now shows terms for testing purposes.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,36 @@
-const About = () => (
-	<main role="main">
-		<div>
-			<h1>About</h1>
-			<p>Starter kit for full-stack JavaScript projects. For more information, see the wiki:</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
-		</div>
-	</main>
-);
+import { useEffect, useState } from "react";
+
+const About = () => {
+	const [results, setResults] = useState([]);
+	useEffect(() => {
+		fetch("/api/terms")
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then(results => {
+				setResults(results);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	return (
+		<main role="main">
+			<div>
+				<h1>Terms</h1>
+				<ul>
+				{ results.map(term => 
+					<li key={term.id}><b>{term.term}</b> - {term.definition}</li>	
+				  )
+				}
+				</ul>
+			</div>
+		</main>
+	);
+};
 
 export default About;
